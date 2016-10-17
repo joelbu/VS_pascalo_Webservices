7bafd970931e1ecd3427039ee9040b2c721422d2
Joel Busch <joel.busch@hotmail.com>
2016-10-17 17:40:36 +0200

    Added some of the boring stuff, activity titles, buttons, button listener code etc, made a raw HTTP request that passes the unit test and a real world test

--- app/src/main/java/ch/ethz/inf/vs/a2/pascalo/vs_pascalo_webservices/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/pascalo/vs_pascalo_webservices/MainActivity.java
@@ -1,13 +1,43 @@
 package ch.ethz.inf.vs.a2.pascalo.vs_pascalo_webservices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_rest_client).setOnClickListener(this);
+        findViewById(R.id.button_soap_client).setOnClickListener(this);
+        findViewById(R.id.button_rest_server).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_rest_client:
+                startActivity(new Intent(getApplicationContext(), REST_client.class));
+                break;
+            case R.id.button_soap_client:
+                startActivity(new Intent(getApplicationContext(), SOAP_client.class));
+                break;
+            case R.id.button_rest_server:
+                startActivity(new Intent(getApplicationContext(), REST_server.class));
+                break;
+            default:
+                Log.e(TAG, "onClick got called with an unexpected view.");
+                finish();
+                break;
+        }
+
     }
 }
--- app/src/main/java/ch/ethz/inf/vs/a2/solution/http/HttpRawRequestImpl.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/solution/http/HttpRawRequestImpl.java
@@ -9,6 +9,28 @@ import ch.ethz.inf.vs.a2.http.HttpRawRequest;
 public class HttpRawRequestImpl implements HttpRawRequest {
     @Override
     public String generateRequest(String host, int port, String path) {
-        return null;
+
+        /* Builds one long string that gets a useful response when fired from my browser extension,
+        passes the unit test and ends up looking like this:
+
+        GET /sunspots/Spot1/sensors/temperature/ HTTP/1.1
+        Host: vslab.inf.ethz.ch:8081
+        Accept: text/html
+        Cache-Control: no-cache
+        Connection: close
+
+        For the parameters  host = "vslab.inf.ethz.ch"
+                            port = 8081
+                            path = "/sunspots/Spot1/sensors/temperature/"
+
+        I used a StringBuilder because I think that's what you're supposed to do when you keep
+        adding to the same String, because String Objects are immutable and it would cause a lot
+        of unnecessary allocations with the normal + concatenation.*/
+
+        return new StringBuilder(256).append("GET ").append(path).append(" HTTP/1.1\r\n")
+                .append("Host: ").append(host).append(":").append(String.valueOf(port)).append("\r\n")
+                .append("Accept: text/html\r\n")
+                .append("Cache-Control: no-cache\r\n")
+                .append("Connection: close\r\n\r\n").toString();
     }
 }
