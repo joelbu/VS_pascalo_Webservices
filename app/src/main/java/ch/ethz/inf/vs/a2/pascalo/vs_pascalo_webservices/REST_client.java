dfe4589671c94f8ad2170337f6a62ce570a6820b
Joel Busch <joel.busch@hotmail.com>
2016-10-17 22:36:05 +0200

    Did the whole Raw HTTP Request part of the assignment
    Added network access permissions to manifest
    Added minimal threading for network access
    Added sending through the Socket object
    Added parsing the HTML response (only by string search)
    Added an Again-Button for easier testing and repeated feeling of success

--- app/src/main/java/ch/ethz/inf/vs/a2/pascalo/vs_pascalo_webservices/REST_client.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/pascalo/vs_pascalo_webservices/REST_client.java
@@ -1,25 +1,83 @@
 package ch.ethz.inf.vs.a2.pascalo.vs_pascalo_webservices;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import ch.ethz.inf.vs.a2.sensor.SensorListener;
+import ch.ethz.inf.vs.a2.solution.sensor.RawHttpSensor;
 
-public class REST_client extends AppCompatActivity implements SensorListener {
+public class REST_client extends AppCompatActivity implements SensorListener, View.OnClickListener  {
+
+    private final String TAG = "RestClient";
+
+    private RawHttpSensor mRawHttpSensor;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_client);
+
+        // Create RawHttpSensor and make our activity a listener of it, so we can display the result
+        mRawHttpSensor = new RawHttpSensor();
+        mRawHttpSensor.registerListener(this);
+
+        mTextView = (TextView) findViewById(R.id.rest_client_text_view);
+        findViewById(R.id.rest_client_again_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeRawHttpSensorRequest();
+    }
+
+    private void makeRawHttpSensorRequest() {
+        // Network activity needs to be done on another thread
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                String response = "";
+                try {
+                    response = mRawHttpSensor.executeRequest();
+                } catch (Exception e) {
+                    Log.e(TAG, "Caught exception from mRawHttpSensor " + e.toString());
+                }
+                double temperature = mRawHttpSensor.parseResponse(response);
+                return null;
+            }
+        }.execute();
     }
 
     @Override
     public void onReceiveSensorValue(double value) {
-        
+
+        // We need to change UI text on the UI thread so whatever, this works I guess
+        final double finalValue = value;
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.setText(getText(R.string.temperature_is) + " " + String.valueOf(finalValue) + getText(R.string.degree_symbol));
+            }
+        });
+
     }
 
     @Override
     public void onReceiveMessage(String message) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rest_client_again_button:
+                makeRawHttpSensorRequest();
+                break;
+        }
     }
 }
--- app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/RawHttpSensor.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/RawHttpSensor.java
@@ -1,19 +1,75 @@
 package ch.ethz.inf.vs.a2.solution.sensor;
 
+import android.text.Html;
+import android.util.Log;
+
 import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import ch.ethz.inf.vs.a2.solution.http.HttpRawRequestImpl;
 
 /**
  * Created by Joel on 17.10.2016.
  */
 
 public class RawHttpSensor extends AbstractSensor {
+    private final String TAG = "RawHttpSensor";
+
+    private final int mPort = 8081;
+    private final String mHost = "vslab.inf.ethz.ch";
+    private final String mPath = "/sunspots/Spot1/sensors/temperature/";
+
     @Override
     public String executeRequest() throws Exception {
-        return null;
+        Log.d(TAG, "Arrived in executeRequest");
+
+        // Bind to a port etc, pretty automatic, it even does DNS-lookup and everything for us
+        Socket socket = new Socket(mHost, mPort);
+
+        // Build our request with the other class we implemented previously
+        HttpRawRequestImpl requestMaker = new HttpRawRequestImpl();
+        String request = requestMaker.generateRequest(mHost, mPort, mPath);
+
+        // Write the request into the socket thereby sending it to the server
+        PrintWriter printwriter = new PrintWriter(socket.getOutputStream());
+        printwriter.print(request);
+        printwriter.flush();
+
+        // Read the response from the socket
+        char[] response = new char[2048];
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        inputStreamReader.read(response, 0, 2048);
+
+        // Make a String from the character array and return that
+        return String.valueOf(response);
     }
 
     @Override
     public double parseResponse(String response) {
-        return 0;
+        Log.d(TAG, "Arrived in parseResponse");
+        // Since the unit test ensures we set the Accept header to text/html we are going to have
+        // to find the value from within the HTML here, which is frankly ridiculous
+
+        // getterValue is the HTML class attribute of the span containing the value, it is unique
+        // within the page, so it should be safe to search for this
+        int index = response.indexOf("getterValue");
+        String valueText = response.substring(index+13, index+18);
+
+        // check if we were unfortunate and only got one decimal after the period, throw out '<'
+        if (-1 != valueText.indexOf('<')) {
+            valueText = valueText.substring(0, 3);
+        }
+
+        double temperature = Double.valueOf(valueText).doubleValue();
+
+        // This is confusing to me, the assignment tells us to register our activity as a listener,
+        // so I publish the value here, right before returning it and do not use the return value
+        // in the activity but instead the value we get in the listener
+        this.sendValue(temperature);
+        return temperature;
     }
 }
