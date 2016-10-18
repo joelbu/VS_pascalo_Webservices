2342c10124113756ea052c9e1b8943cace29f518
Jakob Meier <jakobmeier@bluewin.ch>
2016-10-18 13:55:33 +0200

    untested change on XmlSensor

--- app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/XmlSensor.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/solution/sensor/XmlSensor.java
@@ -1,19 +1,32 @@
 package ch.ethz.inf.vs.a2.solution.sensor;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+import ch.ethz.inf.vs.a2.http.RemoteServerConfiguration;
 import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
 
 /**
  * Created by Joel on 17.10.2016.
  */
 
-public class XmlSensor extends AbstractSensor {
+public class XmlSensor extends AbstractSensor{
+
+    private final String mPath = "/SunSPOTWebServices/SunSPOTWebservice?wsdl";
+
     @Override
     public String executeRequest() throws Exception {
+        URL url = new URL("http", RemoteServerConfiguration.HOST, RemoteServerConfiguration.SOAP_PORT, mPath);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Accept", "text/xml");
         return null;
     }
 
     @Override
     public double parseResponse(String response) {
+
         return 0;
     }
 }
