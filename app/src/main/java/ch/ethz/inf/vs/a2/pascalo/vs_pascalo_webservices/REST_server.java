a5b9b2b9aec70ecf5661c325a644f75464c50572
Joel Busch <joel.busch@hotmail.com>
2016-10-18 15:59:18 +0200

    Adding a start and stop service button for the server

--- app/src/main/java/ch/ethz/inf/vs/a2/pascalo/vs_pascalo_webservices/REST_server.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/pascalo/vs_pascalo_webservices/REST_server.java
@@ -3,6 +3,8 @@ package ch.ethz.inf.vs.a2.pascalo.vs_pascalo_webservices;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.net.NetworkInterface;
 import java.util.Enumeration;
@@ -11,14 +13,20 @@ import ch.ethz.inf.vs.a2.sensor.SensorListener;
 
 import static java.net.NetworkInterface.getNetworkInterfaces;
 
-public class REST_server extends AppCompatActivity {
+public class REST_server extends AppCompatActivity implements View.OnClickListener{
+
     private static String TAG = "RESTServer";
-    NetworkInterface mWlanInterface;
+    private NetworkInterface mWlanInterface;
+    private TextView mTextViewServiceStatus;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_server);
+
+        mTextViewServiceStatus = (TextView) findViewById(R.id.rest_server_status_text);
+        findViewById(R.id.rest_server_start).setOnClickListener(this);
+        findViewById(R.id.rest_server_stop).setOnClickListener(this);
 
         Enumeration<NetworkInterface> interfaces;
 
@@ -45,4 +53,15 @@ public class REST_server extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rest_server_start:
+
+                break;
+            case R.id.rest_server_stop:
+
+                break;
+        }
+    }
 }
